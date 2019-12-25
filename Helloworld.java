ef2cc78e32b9a3999d064d857ac0b86ebae9f099
root <root@instance-3.us-central1-a.c.model-craft-237012.internal>
2019-12-25 16:14:22 +0000

    Git polling testing

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 public class Helloworld {
 	public	static void main ( String[] args)
 	{
-		System.out.println("Hellow World ");
+		System.out.println("Hellow World ,  I am back again with new commit");
 
 	}
 }
