52764c34f292f6a4042a671920750f898a7927be
root <root@instance-3.us-central1-a.c.model-craft-237012.internal>
2019-12-25 16:29:40 +0000

    Again testing, last testing

--- Helloworld.java
+++ Helloworld.java
@@ -1,11 +1,8 @@
 public class Helloworld {
 	public	static void main ( String[] args)
 	{
-<<<<<<< HEAD
-		System.out.println("Hellow World ,  I am back again with new commit, Lets do it again");
-=======
-		System.out.println("Hello Priyanka  ");
->>>>>>> 571679b330dbb5851fe5dea51407c5d8821e51f0
 
+		System.out.println("Hellow World ,  I am back again with new commit, Lets do it again");
+		System.out.println("Hello Priyanka  ");
 	}
 }
